c224aaf4cc59029467a80b2c8d6b69075f03531e
SulakshiWasana <nimeshasulakshiwasana@gmail.com>
2022-08-09 22:43:35 +0530

    Upload Assignment

---
+++ src/app/App.js
@@ -0,0 +1,35 @@
+// import React from "react";
+// import Login from "../pages/session/login";
+//
+// function App() {
+//     return (
+//       <Login></Login>
+//
+//
+//     );
+// }
+//
+// export default App;
+
+import NavBar from "../components/common/NavBar";
+
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Product from "../pages/Product";
+
+function App() {
+    return (<>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Dashboard/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/product" element={<Product/>}/>
+                <Route path="/cart" element={<Dashboard/>}/>
+
+            </Routes>
+        </>
+
+    );
+}
+
+export default App;
